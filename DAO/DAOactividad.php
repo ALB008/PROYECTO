c73dd157684dcdaf029f1406fc2707fa684a2217
ALB008 <valentinamg1022@hotmail.com>
2022-03-16 17:49:27 -0500

    Objetos y conexion a la base de datos nf

---
+++ DAO/DAOactividad.php
@@ -0,0 +1,23 @@
+<?php
+include_once "conexion.php";
+include "actividad.php";
+
+class DAOactividad extends conectar{
+    public function createActividad(){
+        $c= new conectar();
+        $conexion=$c->conexion();
+        $sql = "call createActividad('$datos[0]', '$datos[1]', '$datos[2]', '$datos[3]', '$datos[4]', '$datos[5]', '$datos[6]')";
+        return $result=mysqli_query($conexion,$sql);
+    }
+
+    public function updateActividad(){
+
+    }
+
+    public function deleteActividad(){
+        
+    }
+}
+
+
+?>
