f5b83d9cf231aff7ffc1f943bef0039be964a409
ALB008 <valentinamg1022@hotmail.com>
2022-03-19 14:35:46 -0500

    Verificacion de campos funcional

--- Register/activity.php
+++ Register/activity.php
@@ -1,5 +1,4 @@
 <?php
-    //require_once "Pagina/util/conexion.php";
     if(isset($_POST['submit'])){
         $nom_activ=$_POST['nombre'];
         $desc_activ=$_POST['desc'];
@@ -8,73 +7,145 @@
         $hora_activ=$_POST['hora'];
         $limit_activ=$_POST['limit'];
         $dura_activ=$_POST['dura'];
-        $x=0;
     }
-    /*
-    $obj = new conectar();
-    $conexion=$obj->conexion();
-    $id=$_GET['Id_usu'];
-    $sql="SELECT Id_usu,Nom_usu,Nacim_usu,Tel_usu,Cor_usu FROM usuario WHERE Id_usu='$id'";
-    $result=mysqli_query($conexion,$sql);
-    $ver=mysqli_fetch_row($result);
-    */
 ?>
+
 
 <!DOCTYPE html>
-<html lang="es">
-    <head>
-        <title>Nueva actividad</title>
-        <meta charset="utf-8">
-    </head>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Crear Actividad</title>
+	<link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="css/estilos.css">
+
+</head>
     <body>
-        <form action="<?php 
-        htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
-        <h1>Completa la información para <br> crear una nueva actividad!</h1>
-        <label for="">Nombre:</label> <br>
-        <input type="text" name="nombre" value="<?php
-        if(isset($nom_activ)){echo $nom_activ;}?>"> <br> <br>
-        <label for="">Breve descripción: </label> <br>
-        <input type="text" name="desc" value="<?php 
-        if(isset($desc_activ)){echo $desc_activ;}?>"> <br> <br>
-        <label for="">Ubicación </label> <br>
-        <input type="text" name="ubi" value="<?php
-        if(isset($ubi_activ)){echo $ubi_activ;}?>"> <br><br>
-        <label for="">Día de realización: </label><br>
-        <input type="text" name="dia" value="<?php
-        if(isset($dia_activ)){echo $dia_activ;}?>"> <br><br>
-        <label for="">Hora: </label><br>
-        <input type="text" name="hora" value="<?php
-        if(isset($hora_activ)){echo $hora_activ;}?>"> <br><br>
-        <label for="">Limite de participantes</label><br>
-        <input type="text" name="limit" value="<?php
-        if(isset($limit_activ)){echo $limit_activ;}?>"><br><br><br>
-        <label for="">Duración:</label><br>
-        <input type="text" name="dura" value="<?php
-        if(isset($dura_activ)){echo $dura_activ;}?>"><br><br><br>
+        <main>
+            <form action="" method="post" id="formulario" class="formulario">
+                
+                <!-- Grupo: Nombre -->
+                <div class="formulario__grupo" id="grupo__nombre">
+                    <label for="nombre" class="formulario__label">Nombre:</label>
+                    <div class="formulario__grupo-input">
+                    <input type="text" class="formulario__input" id="nombre" name="nombre" value="<?php
+                    if(isset($nom_activ)){echo $nom_activ;}?>">
+                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                    </div>
+                    <p class="formulario__input-error">Rellena este campo solo con letras y números</p>
+                </div>
+                <!-- ///////////  -->
+
+                <!-- Grupo: Desc -->
+                <div class="formulario__grupo" id="grupo__desc">
+                    <label for="desc" class="formulario__label">Breve descripción: </label>
+                    <div class="formulario__grupo-input">
+                    <input type="textarea" class="formulario__input" id="desc" name="desc" value="<?php 
+                    if(isset($desc_activ)){echo $desc_activ;}?>">
+                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                    </div>
+                    <p class="formulario__input-error">Rellena este campo con menos de 200 palabras y solo con números y letras</p>
+                </div>
+                <!-- ///////////  -->
+
+                <!-- Grupo: Ubi -->
+                <div class="formulario__grupo" id="grupo__ubi">
+                    <label for="ubi" class="formulario__label">Ubicación: </label>
+                    <div class="formulario__grupo-input">
+                    <input type="text" class="formulario__input" id="ubi" name="ubi" value="<?php 
+                    if(isset($ubi_activ)){echo $ubi_activ;}?>">
+                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                    </div>
+                    <p class="formulario__input-error">Rellena este campo solo con números y letras</p>
+                </div>
+                <!-- ///////////  -->
+
+                <!-- Grupo: Dia -->
+                <div class="formulario__grupo" id="grupo__dia">
+                    <label for="dia" class="formulario__label">Día de realización:
+                    <div class="formulario__grupo-input"> <br>
+                    <!--
+                    <select name="dia de la semana">
+                        <option class="form__input" name="dia" id="dia1" value="L">Lunes</option>
+                        <option class="form__input" name="dia" id="dia2" value="M">Martes</option>
+                        <option class="form__input" name="dia" id="dia3" value="X">Miercoles</option>
+                        <option class="form__input" name="dia" id="dia4" value="J">Jueves</option>
+                        <option class="form__input" name="dia" id="dia5" value="V">Viernes</option>
+                    </select>
+                    -->
+                    <!--
+                    <input type="radio" class="form__input" name="dia" id="dia1">Lunes 
+                    <input type="radio" class="form__input" name="dia" id="dia2">Martes 
+                    <input type="radio" class="form__input" name="dia" id="dia3">Miercoles 
+                    <input type="radio" class="form__input" name="dia" id="dia4">Jueves 
+                    <input type="radio" class="form__input" name="dia" id="dia5">Viernes
+                    -->
+                    <input type="text" class="formulario__input" id="dia" name="dia" value="<?php 
+                    if(isset($dia_activ)){echo $dia_activ;}?>">
+                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                    </div>
+                    </label>
+                    <p class="formulario__input-error">Rellena este campo solo con letras</p>
+                </div>
+                <!-- ///////////  -->
+
+                <!-- Grupo: Hora -->
+                <div class="formulario__grupo" id="grupo__hora">
+                    <label for="hora" class="formulario__label">Hora: </label>
+                    <div class="formulario__grupo-input">
+                    <input required type="time" class="formulario__input" id="hora" name="hora" value="<?php 
+                    if(isset($hora_activ)){echo $hora_activ;}?>">
+                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                    </div>
+                    <p class="formulario__input-error">Rellena este campo ingresando la hora</p>
+                </div>
+                <!-- ///////////  -->
+
+                <!-- Grupo: Limite -->
+                <div class="formulario__grupo" id="grupo__limit">
+                    <label for="limit" class="formulario__label">Limite de participantes: </label>
+                    <div class="formulario__grupo-input">
+                    <input type="text" class="formulario__input" id="limit" name="limit" value="<?php 
+                    if(isset($limit_activ)){echo $limit_activ;}?>">
+                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                    </div>
+                    <p class="formulario__input-error">Rellena este campo solo con números de máximo dos cifras</p>
+                </div>
+                <!-- ///////////  -->
+                
+                <!-- Grupo: Duración -->
+                <div class="formulario__grupo" id="grupo__dura">
+                    <label for="dura" class="formulario__label">Duración en horas: </label>
+                    <div class="formulario__grupo-input">
+                    <input type="text" class="formulario__input" id="dura" name="dura" value="<?php 
+                    if(isset($dura_activ)){echo $dura_activ;}?>">
+                    <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                    </div>
+                    <p class="formulario__input-error">Rellena este campo solamente con números</p>
+                </div>
+                <!-- ///////////  -->
+
+                
+
+                <div class="formulario__mensaje" id="formulario__mensaje">
+				<p><i class="fas fa-exclamation-triangle"></i> <b>Error:</b> Por favor rellena el formulario correctamente. </p>
+			</div>
+
+			<div class="formulario__grupo formulario__grupo-btn-enviar">
+				<button type="submit" class="formulario__btn">Enviar</button>
+				<p class="formulario__mensaje-exito" id="formulario__mensaje-exito">Formulario enviado exitosamente!</p>
+			</div>
+
+                
+            </form>
+        </main>
+
+        <script src="js/formularioA.php"></script>
+	    <script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+        
 
 
-        <input type="submit" name="submit">
-
-<?php
-ini_set('include_path', '/objects/actividad.php');
-ini_set('include_path', '/util/DAOactividad.php');
-$activ = new actividad();
-$R_activ = new DAOactividad();
-$R_activ->createActividad($activ);
-/*
-$datos=array($cod_lib,$nombre_lib,$estado_lib,$autor_lib,$genero_lib,$editorial_lib,$anno_lib,$paginas_lib,$isbn_lib);
-        $obj = new libro();
-        if ($obj->create($datos)==1){
-            header("location: libros.php");
-        } else{
-            echo "<br>Fallo al insertar";
-        }
-        */
-?>
-
-    
-   
-    
-    </form>
     </body>
 </html>
---
+++ Register/js/formularioA.php
@@ -0,0 +1,116 @@
+<script>
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	texto1: /^[[a-zA-ZÀ-ÿ\s0-9\ \_\-]{4,50}$/, // Letras, numeros, guion y guion_bajo
+	texto2: /^[a-zA-ZÀ-ÿ\s0-9]{1,200}$/, // Letras y espacios, pueden llevar acentos.
+	num: /^[1-9]{1,2}$/, // 1 a 2 digitos.
+	num2: /^[1-9]{1}$/,
+	
+}
+
+const campos = {
+	nombre: false,
+	desc: false,
+	ubi: false,
+	dia: false,
+	hora: false,
+	limit: false,
+	dura: false
+	
+}
+
+const ValidarFormulario = (e) => {
+	switch(e.target.name) {
+		case "nombre":
+			ValidarCmpo(expresiones.texto1, e.target, 'nombre');
+		break;		
+		
+		case "desc":
+			ValidarCmpo(expresiones.texto2, e.target, 'desc');		
+		break;
+
+		case "ubi":
+			ValidarCmpo(expresiones.texto1, e.target, 'ubi');
+		break;
+
+		case "dia":
+			ValidarCmpo(expresiones.texto1, e.target, 'dia');
+		break;
+
+		case "hora":
+		break;
+
+		case "limit":
+			ValidarCmpo(expresiones.num, e.target, 'limit');
+		break;
+
+		case "dura":
+			ValidarCmpo(expresiones.num2, e.target, 'dura');
+		break;
+
+	}
+}
+
+const ValidarCmpo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campo[campo] = false;
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', ValidarFormulario);
+	input.addEventListener('blur', ValidarFormulario);
+});
+
+formulario.addEventListener('submit', (e) =>{
+	e.preventDefault();
+
+	if( campos.nombre && campos.desc && campos.ubi && campos.dia && campos.limit && campos.dura){
+		formulario.reset();
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() =>{
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		},5000);
+
+		include_once "DAO/DAOactividad.php";
+            $id_usu=$_POST['id'];
+            $nombre_usu=$_POST['nombre'];
+            $nacim_usu=$_POST['nacim'];
+            $tel_usu=$_POST['tel'];
+            $correo_usu=$_POST['correo'];
+            $x=0;
+
+            $datos=array($id_usu,$nombre_usu,$nacim_usu,$tel_usu,$correo_usu);
+            $obj= new usuario();
+            if ($obj->create($datos)==1){
+                header("location: Usuarios.php");
+            } else{
+                echo "<br>Fallo al insertar";
+            }
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+	} else{
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+		setTimeout(() =>{
+			document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
+		},5000);
+	}
+});
+</script>
